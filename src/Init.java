2d391e81694d81bc7561f2345b2d186e126a3351
Cedo <humanpriori@gmail.com>
2018-11-19 21:42:36 -0500

    Initial commit

---
+++ src/Init.java
@@ -0,0 +1,6 @@
+public class Init {
+    public static void main (String[] args)
+    {
+        System.out.println("the output of the program for you to see is going to be the word fuck. Hello world.");
+    }
+}
---
+++ src/Inity.java
@@ -0,0 +1,7 @@
+public class Inity {
+
+    public static void main(String[] args) {
+        System.out.println("the output of the program for you to see is going to be the word fucky. Hello world.");
+    }
+
+}
